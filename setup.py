e07ca0558d546eeb68e340b46aa7a9b53d66c336
Jay-Kanakia-37 <joykanakia20061994@gmail.com>
2025-10-28 13:10:01 +0530

    cookiecutter initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This app will recommend 3 items from swiggy',
+    author='Jay Kanakia',
+    license='',
+)
